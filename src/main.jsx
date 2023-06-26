09e7c5542735c1385682816924306f12c4050699
Alberto <albertoavv@hotmail.es>
2023-06-25 20:53:06 -0400

    redux toolkit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { store } from './store'
+import { Provider } from 'react-redux'
+import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './todoApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <App /> */}
+      {/* <PokemonApp /> */}
+      <TodoApp />
+    </Provider>
+  </React.StrictMode>,
+)
